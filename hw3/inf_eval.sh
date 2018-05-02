8aa2d6fc21675cc9fca74c3365d5c24adf3ef557
chhiilnos <b03901070@ntu.edu.tw>
2018-05-02 15:37:38 +0800

    user-defined filepath

--- hw3/inf_eval.sh
+++ hw3/inf_eval.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 echo $1
+mkdir $3
 mkdir report/$1
 mkdir report/$1/mask
-python3 inference.py data/validation report/$1/mask $1 50
-python3 mean_iou_evaluate.py -g data/validation -p report/$1/mask > report/$1/meaniou.txt
+python3 inference.py $2 report/$1/mask $1 50
+cp report/$1/mask/* $3/
 
