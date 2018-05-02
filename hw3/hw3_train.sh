d388b0f955e297602c435bdb5add5f7ef3809ab0
chhiilnos <b03901070@ntu.edu.tw>
2018-05-02 15:25:45 +0800

    add python3 mask_pre.py to hw3_train.sh

--- hw3/hw3_train.sh
+++ hw3/hw3_train.sh
@@ -1,3 +1,4 @@
 mkdir weight
 wget "https://www.dropbox.com/s/9urkq5gbt1wfd7x/vgg16_weights_tf_dim_ordering_tf_kernels.h5?dl=0" -O weight/vgg16_weights_tf_dim_ordering_tf_kernels.h5
+python3 mask_pre.py
 bash train.sh FCN_Vgg16_32s
